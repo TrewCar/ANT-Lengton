bdceb7b0ed973a546209b210d5120e02dc53ac4e
TrewCar <113162792+TrewCar@users.noreply.github.com>
2022-11-13 14:31:38 +0300

    Add files via upload

---
+++ AntLengton/Form1.Designer.cs
@@ -0,0 +1,96 @@
+﻿namespace AntLengtonOptimozation
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(13, 13);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(500, 500);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(537, 13);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Старт";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(537, 52);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Стоп";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(622, 526);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ AntLengton/Form1.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace AntLengtonOptimozation
+{
+    public partial class Form1 : Form
+    {
+        Graphics g;
+        Bitmap bmp = new Bitmap(500, 500);
+
+        static int ColVo = 100;
+
+        static readonly int rect = 500 / ColVo;
+
+        int xPos = 50;
+        int yPos = 50;
+
+        int[,] LocInf = new int[ColVo, ColVo];
+
+        readonly int[,] Posin = new int[,]
+        {
+            {0,-1},
+            {-1,0},
+            {0,1},
+            {1,0},
+        };
+
+        static int MovePos = 0;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            pictureBox1.Image = bmp;
+
+            g = Graphics.FromImage(pictureBox1.Image);
+
+            for (int i = 0; i < 500; i += rect)
+            {
+                g.DrawLine(new Pen(Color.Black, 1), new Point(i, 0), new Point(i, pictureBox1.Height));
+            }
+            g.DrawLine(new Pen(Color.Black, 1), new Point(pictureBox1.Width - 1, 0), new Point(pictureBox1.Width - 1, pictureBox1.Height));
+            for (int i = 0; i < 500; i += rect)
+            {
+                g.DrawLine(new Pen(Color.Black, 1), new Point(0, i), new Point(pictureBox1.Width, i));
+            }
+            g.DrawLine(new Pen(Color.Black, 1), new Point(0, pictureBox1.Height - 1), new Point(pictureBox1.Width, pictureBox1.Height - 1));
+
+
+
+        }
+
+        private void ProvPos()
+        {
+
+            if (xPos == ColVo)
+                xPos = 0;
+
+            else if (xPos < 0)
+                xPos = ColVo - 1;
+
+            if (yPos == ColVo)
+                yPos = 0;
+
+            else if (yPos < 0)
+                yPos = ColVo - 1;
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            
+            xPos += Posin[MovePos,0];
+            yPos += Posin[MovePos, 1];
+
+            ProvPos();
+
+            if (LocInf[xPos, yPos] == 1)
+            {
+                LocInf[xPos, yPos] = 0;
+                MovePos -= 1;
+                g.FillRectangle(Brushes.White, xPos * rect, yPos * rect, rect, rect);
+            }
+            else if (LocInf[xPos, yPos] == 0)
+            {
+                LocInf[xPos, yPos] = 1;
+                MovePos += 1;
+                g.FillRectangle(Brushes.Blue, xPos * rect, yPos * rect, rect, rect);
+            }
+
+            if (MovePos == 4)
+                MovePos = 0;
+            else if (MovePos == -1)
+                MovePos = 3;
+
+            pictureBox1.Refresh();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            timer1.Start();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            timer1.Stop();
+        }
+    }
+}
